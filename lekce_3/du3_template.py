73c209b5b307a5f2808fe287caf87e266f132978
LubaKubi <lubakubi@gmail.com>
2025-03-13 22:13:05 +0100

    motory vpred 1s a stop 1s a motory vzad 1s a stop :-)

--- lekce_3/du3_template.py
+++ lekce_3/du3_template.py
@@ -11,14 +11,42 @@ def jed(strana, smer, rychlost):
     # Rychlost je celočíselné číslo od 0-255
     # vyuzijte prikladu z hodiny, ktery poslal povel x03 - prave kolo pro jizdu rovne
     # ostatni povely:
-    # Pravý motor:
-    # 0x02 - příkaz pro pohyb vzad
-    # 0x03 - příkaz pohyb vpřed
+
     # Levý motor:
     # 0x04 - příkaz pro pohyb vzad
     # 0x05 - příkaz pro pohyb vpřed
 
-    i2c.write(0x70, b'\x03' + bytes([rychlost]))
+    if (strana == "leva"):
+       print("leva")
+       if smer == "dopredu":
+            print("dopredu")
+            i2c.write(0x70, b'\x05' + bytes([rychlost]))
+       elif smer == "dozadu":
+            print("dozadu")
+            i2c.write(0x70, b'\x04' + bytes([rychlost]))
+       else:
+            print("nemam smer")
+
+    elif strana == "prava":
+    # Pravý motor:
+    # 0x02 - příkaz pro pohyb vzad
+    # 0x03 - příkaz pohyb vpřed
+
+       print("prava")
+       if smer == "dopredu":
+            print("dopredu")
+            i2c.write(0x70, b'\x03' + bytes([rychlost]))
+       elif smer == "dozadu":
+            print("dozadu")
+            i2c.write(0x70, b'\x02' + bytes([rychlost]))
+       else:
+           print("nemam smer")
+    else:
+        print("nemam stranu")
+
+
+
+
 
 if __name__ == "__main__":
     # Write your code here :-)
@@ -26,6 +54,20 @@ if __name__ == "__main__":
     init_motoru()
     # volejte funkci jed, tak abyste ziskali:
     # Pohyb robota dopredu 1s
+    jed("leva","dopredu",100)
+    jed("prava","dopredu",100)
+    sleep(1000)
     # Zastaveni 1s - DULEZITE! Nikdy nemente smer jizdy bez zastaveni
+    jed("leva","dopredu",0)
+    jed("prava","dopredu",0)
+    sleep(1000)
+
     # Pohyb vzad 1s,
+    jed("leva","dozadu",100)
+    jed("prava","dozadu",100)
+    sleep(1000)
+    # Zastaveni 1s - DULEZITE! Nikdy nemente smer jizdy bez zastaveni
+    jed("leva","dozadu",0)
+    jed("prava","dozadu",0)
+    sleep(1000)
     # zastaveni

f2fd3642c8746d1c01c0fcab787deee127161376
DaveMcFly-dev <david@flekna.cz>
2025-03-17 20:17:43 +0100

    .

--- code.py
+++ code.py
@@ -1,15 +1,23 @@
-from board import P14, P15
-from digitalio import DigitalInOut
-from time import sleep
+from picoed import i2c
+
+def byte_na_bity(buffer):
+    data_int = int.from_bytes(buffer, "big")
+    data_bit_string = bin(data_int)
+    return data_bit_string
 
 if __name__ == "__main__":
 
-    levy_enkoder = DigitalInOut(P14)
-    pravy_enkoder = DigitalInOut(P15)
+    while not i2c.try_lock():
+        pass
 
-    while True:
-        surova_data_leva = int(levy_enkoder.value)
-        surova_data_prava = int(pravy_enkoder.value)
-        print(surova_data_leva, surova_data_prava)
-        sleep(0.1)
-        
+    # pokud se program dostane sem, tak se i2c podarilo zamknout
+    try:
+        buffer = bytearray(1)
+        i2c.readfrom_into(0x38, buffer, start = 0, end = 1)
+        print(buffer)
+        data_bit_string = byte_na_bity(buffer)
+        print(data_bit_string)
+        for i in range(0, len(data_bit_string)):
+            print(data_bit_string[i])
+    finally:
+        i2c.unlock()

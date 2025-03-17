9deb8de6b16ba5fab22bd04d38c0cc5612f8c850
LubaKubi <lubakubi@gmail.com>
2025-03-17 23:18:40 +0100

    počet tiků od startu programu /pro leve a prave kolo

--- lekce_4/du4_template.py
+++ lekce_4/du4_template.py
@@ -1,18 +1,31 @@
 from microbit import sleep, pin14, pin15
 
+posledni_stav_levy = 0
+posledni_stav_pravy = 0
+tik_count_levy = 0
+tik_count_pravy = 0
+
+
 def pocet_tiku_levy():
+    global posledni_stav_levy
+    global tik_count_levy
     surova_data_leva = pin14.read_digital()
-    #zde napiste vas kod
-    #scitejte tiky pro levy enkoder od zacatku behu progamu
-    return #vratte soucet
+    if surova_data_leva != posledni_stav_levy:
+        tik_count_levy += 1
+    posledni_stav_levy = surova_data_leva
+    return tik_count_levy
 
 def pocet_tiku_pravy():
+    global posledni_stav_pravy
+    global tik_count_pravy
     surova_data_prava = pin15.read_digital()
-    #zde napiste vas kod
-    #scitejte tiky pro pravy enkoder od zacatku behu progamu
-    return #vratte soucet
+    if surova_data_prava != posledni_stav_pravy:
+        tik_count_pravy += 1
+    posledni_stav_pravy =  surova_data_prava
+    return tik_count_pravy
 
 if __name__ == "__main__":
+
 
     while True:
         print(pocet_tiku_levy(), pocet_tiku_pravy())

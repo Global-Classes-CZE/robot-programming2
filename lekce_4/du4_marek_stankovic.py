dc8df78caa9ee89c97f3942c227f4cc90dd3dd83
Marek Stankovic <marek.stankovic@gmail.com>
2025-03-21 20:49:06 +0100

    DU4 som prerobil na globalne premenne. Saskovanie s podavanim si lokalnych sa mi nepacilo. Doplnena DU5

--- lekce_4/du4_marek_stankovic.py
+++ lekce_4/du4_marek_stankovic.py
@@ -1,53 +1,48 @@
 from microbit import sleep, pin14, pin15
 from utime import ticks_us, ticks_diff
 
+tiky_leva = 0                           # globalna premenne pocitadla L/P
+prev_data_leva = pin14.read_digital()   # globalna premenna stavu predosleho merania L/P
 
-def pocet_tiku_levy(tiky_leva, stav_leva):
+tiky_prava = 0
+prev_data_prava = pin15.read_digital()
 
-    sum_leva = tiky_leva
-    prev_data_leva = stav_leva
+def pocet_tiku_levy():
 
     surova_data_leva = pin14.read_digital()
 
+    global tiky_leva
+    global prev_data_leva
+
     if surova_data_leva != prev_data_leva:
-        sum_leva += 1
+        tiky_leva += 1
 
     prev_data_leva = surova_data_leva
 
-    return sum_leva, prev_data_leva
+def pocet_tiku_pravy():
 
-def pocet_tiku_pravy(tiky_prava, stav_prava):   #poslal som si lokalne premenne z __main__
+    surova_data_prava = pin15.read_digital()    #precitam enkoder a zapisem stav
 
-    sum_prava = tiky_prava                      #lokalnu premennu v pocitadle sum_prava zosynchronizujem s tiky_prava; lebo tato lokalna premenna sa vynuluje vzdy, ked sa zavola funkcia ak to neurobim
-    prev_data_prava = stav_prava                #rovnako zosycnhronizujem predosly stav enkodera
+    global tiky_prava                           #deklaracia globalnych premennych
+    global prev_data_prava
 
-    surova_data_prava = pin15.read_digital()    #precitam enkoder znova a zapisem stav
+    if surova_data_prava != prev_data_prava:    #porovnavam aktualny stav a predosly
+        tiky_prava += 1                         #ked sa po sebe iduce stavy lisia, inkremantujem pocitadlo
 
-    if surova_data_prava != prev_data_prava:    #ked su zname aspon 2 stavy, porovnavam
-        sum_prava += 1                          #ked sa po sebe iduce stavy lisia, inkremantujem pocitadlo
-
-    prev_data_prava = surova_data_prava         #pre dalsie porovnanie si odlozim predoslu hodnotu enkodera
-
-    return sum_prava, prev_data_prava           #z funkcie vratim lokalne pocitadlo a predosly stav, ktore sa zapisu do lokalnych premennych v maine
+    prev_data_prava = surova_data_prava         #pre dalsie porovnanie si odlozim do predoslej hodnoty cerstvo nameranu hodnotu enkodera
 
 if __name__ == "__main__":
 
     cas_minule = ticks_us()                     #setnem si aktualny cas
 
-    tiky_prava = 0                              #vynulujem pocitadlo
-    stav_prava = pin15.read_digital()           #setnem pociatocny stav praveho enkodera
-
-    tiky_leva = 0
-    stav_leva = pin14.read_digital()
-
     while True:
-
-        tiky_prava, stav_prava = pocet_tiku_pravy(tiky_prava, stav_prava) #idem inkrementovat stav lokalnej premennej tiky_prava a ulozim si aj aktualny stav posledneho citania hodnoty enkodera; hodnoty posielam do funkcie
-        tiky_leva, stav_leva = pocet_tiku_levy(tiky_leva, stav_leva)
 
         cas_ted = ticks_us()
 
-        if ticks_diff(cas_ted, cas_minule) > 500000:                      # porovnavam casy, aby som vypisal hodnoty enkoderov iba raz za 500ms do terminalu
-            print("Leva:",tiky_leva,"Prava:", tiky_prava)
+        pocet_tiku_levy()                       # volam funkcie na meranie enkoderov, porovnanie stavov a zapis do pocitadla
+        pocet_tiku_pravy()
+
+        if ticks_diff(cas_ted, cas_minule) > 500000:                    # porovnavam casy, aby som vypisal hodnoty enkoderov iba raz za 500ms do terminalu
+            print("Leva:",tiky_leva,"Prava:", tiky_prava)               # vypisem stavy pocitadiel L/P
             cas_minule = cas_ted
 
--- lekce_5/du5_template.py
+++ lekce_5/du5_template.py
@@ -1,31 +1,70 @@
-from microbit import sleep, pin14, pin15
+from microbit import sleep, pin14, pin15, i2c
+from utime import ticks_us, ticks_diff
+
+tiky_leva = 0                           # globalna premenne pocitadla L/P
+prev_data_leva = pin14.read_digital()   # globalna premenna stavu predosleho merania L/P
+
+tiky_prava = 0
+prev_data_prava = pin15.read_digital()
 
 def pocet_tiku_levy():
+
     surova_data_leva = pin14.read_digital()
-    #zde napiste vas kod
-    #scitejte tiky pro levy enkoder od zacatku behu progamu
-    return #vratte soucet
+
+    global tiky_leva
+    global prev_data_leva
+
+    if surova_data_leva != prev_data_leva:
+        tiky_leva += 1
+
+    prev_data_leva = surova_data_leva
 
 def pocet_tiku_pravy():
-    surova_data_prava = pin15.read_digital()
-    #zde napiste vas kod
-    #scitejte tiky pro pravy enkoder od zacatku behu progamu
-    return #vratte soucet
 
-def vypocti_rychlost(pocet_tiku):
-    # zde patri ukol DU5
-    
-    return #vratte rychlost v radianech za sekundu
-    
+    surova_data_prava = pin15.read_digital()    #precitam enkoder a zapisem stav
+
+    global tiky_prava                           #deklaracia globalnych premennych
+    global prev_data_prava
+
+    if surova_data_prava != prev_data_prava:    #porovnavam aktualny stav a predosly
+        tiky_prava += 1                         #ked sa po sebe iduce stavy lisia, inkremantujem pocitadlo
+
+    prev_data_prava = surova_data_prava         #pre dalsie porovnanie si odlozim do predoslej hodnoty cerstvo nameranu hodnotu enkodera
+
+def vypocti_rychlost(perioda):
+
+    global tiky_prava
+
+    rychlost_prava = (tiky_prava/40 * 6.283)/(perioda/1000)
+
+    return rychlost_prava
+
 if __name__ == "__main__":
 
+    #test motora
+    #i2c.init()
+    #i2c.write(0x70, b'\x00\x01')
+    #i2c.write(0x70, b'\xE8\xAA')
+    #sleep(100)
+    #i2c.write(0x70, b'\x03' + bytes([0]))
+
     aktualni_rychlost = 0
-    
+    cas_minule_print = ticks_us()              # setnem si aktualny cas
+    perioda = 250                              # perioda merania nastavena na 250ms: nazbieram okolo 25 tickov, co je nad oporucanim 10-20.
+                                               # prislo mi to ako lepsie rozlisenie nez 500, pri zachovani presnosti
+                                               # stale je to celociselny podiel 1 sekundy, co je fajn pri tesrovani a preratavani
     while True:
-        print(pocet_tiku_levy(), pocet_tiku_pravy())
-        #volejte zde funkci aktualni_rychlost = vypocti_rychlost s vhodnou peridou - viz slidy min 166ms;
-        #budete potrebovat vyuzit praci s casem pres tick_us, ticks_diff (mozna prikazy jsou jinak na pico:edu, pokud nenajdete, zeptejte se na discordu)
-        #staci udelat pro jedno kolo
-        
-        print(aktualni_rychlost)
+        cas_ted = ticks_us()
+
+        pocet_tiku_levy()                       # volam funkcie na meranie enkoderov, porovnanie stavov a zapis do pocitadla
+        pocet_tiku_pravy()
+
+        if ticks_diff(cas_ted, cas_minule_print) > perioda*1000:      # porovnavam casy, aby som vypisal hodnoty enkoderov iba raz za 500ms do terminalu
+
+            print("Leva:",tiky_leva,"Prava:", tiky_prava)             # vypisem stavy pocitadiel L/P
+            print("Rychlost:",vypocti_rychlost(perioda),"[rad/s]")
+            tiky_prava = 0
+            cas_minule_print = cas_ted
+
         sleep(5)
+

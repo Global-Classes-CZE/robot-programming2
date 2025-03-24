f95de8aa53391fd789ec35757006de8de8889cb9
Gabriel Lacska <lacska.gabriel@gmail.com>
2025-03-24 19:55:11 +0100

    du5 - Gabriel Lacska

--- lekce_5/du5_template.py
+++ lekce_5/du5_template.py
@@ -1,31 +1,51 @@
 from microbit import sleep, pin14, pin15
 
+
+celkovy_pocet_lava = 0
+celkovy_pocet_prava = 0
+
+predosly_stav_lava = 0
+predosly_stav_prava = 0
+
+counter = 0
+
+aktualna_uhlova_rychlost = 0
+
+
+
 def pocet_tiku_levy():
+    global celkovy_pocet_lava, predosly_stav_lava
     surova_data_leva = pin14.read_digital()
-    #zde napiste vas kod
-    #scitejte tiky pro levy enkoder od zacatku behu progamu
-    return #vratte soucet
+    zmena = 0 if predosly_stav_lava == surova_data_leva else 1
+    predosly_stav_lava = surova_data_leva
+    celkovy_pocet_lava += zmena
+    return celkovy_pocet_lava
 
 def pocet_tiku_pravy():
+    global celkovy_pocet_prava, predosly_stav_prava
     surova_data_prava = pin15.read_digital()
-    #zde napiste vas kod
-    #scitejte tiky pro pravy enkoder od zacatku behu progamu
-    return #vratte soucet
-
-def vypocti_rychlost(pocet_tiku):
-    # zde patri ukol DU5
+    zmena = 0 if predosly_stav_prava == surova_data_prava else 1
+    predosly_stav_prava = surova_data_prava
+    celkovy_pocet_prava += zmena
+    return celkovy_pocet_prava
     
-    return #vratte rychlost v radianech za sekundu
+def vypocitaj_uhlovu_rychlost(pocet_tiku):
+    global aktualna_uhlova_rychlost
+    # zde patri ukol DU5
+    aktualne_otacky = pocet_tiku/40
+    aktualny_uhol = aktualne_otacky*6.28
+    aktualna_uhlova_rychlost = aktualny_uhol*4
     
 if __name__ == "__main__":
 
-    aktualni_rychlost = 0
-    
     while True:
-        print(pocet_tiku_levy(), pocet_tiku_pravy())
-        #volejte zde funkci aktualni_rychlost = vypocti_rychlost s vhodnou peridou - viz slidy min 166ms;
-        #budete potrebovat vyuzit praci s casem pres tick_us, ticks_diff (mozna prikazy jsou jinak na pico:edu, pokud nenajdete, zeptejte se na discordu)
-        #staci udelat pro jedno kolo
+        counter +=1
+        pocet_tiku_levy()
         
-        print(aktualni_rychlost)
+        if counter == 50:
+            vypocitaj_uhlovu_rychlost(pocet_tiku_levy())
+            print (pocet_tiku_levy(), aktualna_uhlova_rychlost)
+            counter=0
+            celkovy_pocet_lava = 0
+            aktualne_otacky = 0
         sleep(5)
